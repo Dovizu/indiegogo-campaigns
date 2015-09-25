94d983c5b9c8c86db7b0c0567464c8c9ec59f144
Donny Reynolds <donnywith@gmail.com>
2015-09-24 23:57:34 -0700

    get basic angular working

--- source/assets/javascripts/all.js
+++ source/assets/javascripts/all.js
@@ -1,4 +1,25 @@
 //= require jquery
-//= require react
-//= require flux
+//= require angular
 //= require_tree .
+
+angular.module('indiegogoCampaigns', [])
+    .factory('campaignFactory', ['$http', function($http) {
+    var indiegogo_api_endpoint = "https://api.indiegogo.com/1/campaigns.json?api_token=e377270bf1e9121da34cb6dff0e8af52a03296766a8e955c19f62f593651b346";
+    var campaignFactory = {};
+    campaignFactory.getAllCampaigns = function () {
+        return $http.get(indiegogo_api_endpoint);
+    };
+    return campaignFactory;
+}])
+    .controller('mainController', ['$scope', 'campaignFactory', function($scope, campaignFactory) {
+        $scope.campaigns = [];
+
+        getCampaigns();
+
+        function getCampaigns() {
+            campaignFactory.getAllCampaigns()
+                .success(function(campaigns) {
+                    $scope.campaigns = campaigns.response;
+                });
+        }
+    }]);
